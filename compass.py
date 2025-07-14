2b82de6a361dfb0281bc6051950731a3525211cb
Samuel Pontefract <zia32kvs@duck.com>
2025-07-14 17:39:53 +0200

    Added 2D compass calculation
    The code now takes the X and Y magnetometer readings and uses them to calculate the true heading, taking declination into account

--- compass.py
+++ compass.py
@@ -1,4 +1,5 @@
 from machine import SoftI2C, Pin
+from math import atan2, pi
 import struct, time
 
 class Magnetometer:
@@ -12,6 +13,7 @@ class Magnetometer:
                     "x-axis data" : 0x01, # Two bytes, LSB -> MSB
                     "y-axis data" : 0x03, # ...
                     "z-axis data": 0x05,  # ...
+                    "axis invert" : 0x29,
                     
                     "status" : 0x09,
                     "control1" : 0x0A,
@@ -30,6 +32,8 @@ class Magnetometer:
         self.qmc5883p.writeto_mem(self.qmc5883p_address, self.registers["control1"], bytes([0x1D]))
         # Setting the module to 2 Gauss range, "Set and Reset On" mode
         self.qmc5883p.writeto_mem(self.qmc5883p_address, self.registers["control2"], bytes([0x0C]))
+        # Inverting the Y-axis (makes it align with how I want)
+        self.qmc5883p.writeto_mem(self.qmc5883p_address, self.registers["axis invert"], bytes([0x01]))
         
         # Setting up the interrupt pin to know when data is ready from the chip
         self.int_pin = Pin(interrupt_pin, Pin.IN)
@@ -58,10 +62,23 @@ class Magnetometer:
     def getdata_raw(self):
         self._update_data()
         return(self.data)
+    
+    def compass_2d(self, declination=0):
+        self._update_data()
+        # Simple calculation of true heading (if you pass in a declination value) assuming the compass is level
+        heading = atan2(self.data[1], self.data[0])*(180/pi) - declination
+        
+        # Ensuring heading values go from 0 to 360, rather than +180 to -180
+        if heading < 0:
+            heading += 360
+        elif heading > 360:
+            heading -= 360
+        
+        return int(heading)
 
 if __name__ == "__main__":
     compass = Magnetometer(46, 3, 1) # drdy could be anything, not used currently
     
     while True:
-        print(compass.getdata_raw())
+        print(compass.compass_2d(1.43))
         time.sleep(1)
