33eefc5d33711feb75e9ff72c1864392e6f693bd
Samuel Pontefract <zia32kvs@duck.com>
2025-07-14 17:01:02 +0200

    Code now includes interrupt handling
    Magnetometer data updated when code detects interrupt from the QMC5883P's DRDY pin.

--- compass.py
+++ compass.py
@@ -18,6 +18,9 @@ class Magnetometer:
                     "control2" : 0x0B,
                     }
         
+        self.data = [0, 0, 0]
+        self.new_data = False
+        
         time.sleep_us(250) # Module needs about 250 microseconds to boot up from power on -> being able to receive I2C comms
         
         self._modulesetup(drdy)
@@ -33,25 +36,31 @@ class Magnetometer:
         self.int_pin.irq(trigger=Pin.IRQ_FALLING, handler=self._newdatahandler())
     
     def _newdatahandler(self):
-        # This will deal with interrupts when there is new data availible
-        pass
+        # This deals with interrupts when there is new data availible
+        self.new_data = True
     
-    def _read_data(self):
-        # Reading all 6 data bytes in one burst
-        data = self.qmc5883p.readfrom_mem(self.qmc5883p_address, self.registers["x-axis data"], 6)
-        # Breaking down the data into each axis' data
-        x_axis = data[:2]
-        y_axis = data[2:4]
-        z_axis = data[4:]
-        # Decoding the bytes data into signed ints, then converting the readings to Gauss
-        x_axis = struct.unpack("<h", x_axis)[0]/15000
-        y_axis = struct.unpack("<h", y_axis)[0]/15000
-        z_axis = struct.unpack("<h", z_axis)[0]/15000
-        
-        return x_axis, y_axis, z_axis
+    def _update_data(self):
+        if self.new_data:
+            # Reading all 6 data bytes in one burst
+            data = self.qmc5883p.readfrom_mem(self.qmc5883p_address, self.registers["x-axis data"], 6)
 
+            # Decoding the bytes data into signed ints, then converting the readings to Gauss
+            x_axis = struct.unpack("<h", data[:2])[0]/15000
+            y_axis = struct.unpack("<h", data[2:4])[0]/15000
+            z_axis = struct.unpack("<h", data[4:])[0]/15000
+            
+            self.data[0] = x_axis
+            self.data[1] = y_axis
+            self.data[2] = z_axis
+            
+            self.new_data = False
+        
+    def getdata_raw(self):
+        self._update_data()
+        return(self.data)
 
 if __name__ == "__main__":
     compass = Magnetometer(46, 3, 5) # drdy could be anything, not used currently
     
-    print(compass._read_data())
+    while True:
+        print(compass.getdata_raw())
