43d7cbfe9cc4b14128ada4ce506bf90e55d16b99
Samuel Pontefract <zia32kvs@duck.com>
2025-07-25 12:00:06 +0200

    Update checking for new data
     No longer uses interrupt pin, now checks the status register to see if there's new data

--- compass.py
+++ compass.py
@@ -3,7 +3,7 @@ from math import atan2, sin, asin, cos, pi
 import struct, time
 
 class Magnetometer:
-    def __init__(self, scl, sda, drdy):
+    def __init__(self, scl, sda):
         self.qmc5883p = SoftI2C(scl=Pin(scl), sda=Pin(sda), freq=400000)
         self.qmc5883p_address = 0x2C
     
@@ -21,48 +21,49 @@ class Magnetometer:
                     }
         
         self.data = [0, 0, 0]
-        self.new_data = False
         
         time.sleep_us(250) # Module needs about 250 microseconds to boot up from power on -> being able to receive I2C comms
         
-        self._modulesetup(drdy)
+        self._modulesetup()
         
-    def _modulesetup(self, interrupt_pin):
+    def _modulesetup(self):
         # Setting the module to Normal power mode, 200Hz data output rate, x4 sensor reads per data output, down sampling = 0 (output every sample)
         self.qmc5883p.writeto_mem(self.qmc5883p_address, self.registers["control1"], bytes([0x1D]))
         # Setting the module to 2 Gauss range, "Set and Reset On" mode
         self.qmc5883p.writeto_mem(self.qmc5883p_address, self.registers["control2"], bytes([0x0C]))
-        
-        # Setting up the interrupt pin to know when data is ready from the chip
-        self.int_pin = Pin(interrupt_pin, Pin.IN)
-        self.int_pin.irq(trigger=Pin.IRQ_FALLING, handler=self._newdatahandler)
-    
-    def _newdatahandler(self, pin):
-        # This deals with interrupts when there is new data availible
-        self.new_data = True
     
     def _update_data(self):
-        if self.new_data:
-            # Reading all 6 data bytes in one burst
-            data = self.qmc5883p.readfrom_mem(self.qmc5883p_address, self.registers["x-axis data"], 6)
-
-            # Decoding the bytes data into signed ints, then converting the readings to Gauss
-            x_axis = struct.unpack("<h", data[:2])[0]/15000
-            y_axis = struct.unpack("<h", data[2:4])[0]/15000
-            z_axis = struct.unpack("<h", data[4:])[0]/15000
-            
-            self.data[0] = x_axis
-            self.data[1] = y_axis
-            self.data[2] = z_axis
-            
-            self.new_data = False
+        counter = 0
         
+        while not(self.qmc5883p.readfrom_mem(0x2C, 0x09, 1)[0] & 0x01): # Checking the DRDY bit of the status register - if no new data, wait a bit then check again
+            time.sleep_us(5) # 1/200 of a second - time it should take for a measurement
+            counter += 1
+            
+            if counter > 2:
+                return None
+            
+        # Reading all 6 data bytes in one burst
+        data = self.qmc5883p.readfrom_mem(self.qmc5883p_address, self.registers["x-axis data"], 6)
+
+        # Decoding the bytes data into signed ints, then converting the readings to Gauss
+        x_axis = struct.unpack("<h", data[:2])[0]/15000
+        y_axis = struct.unpack("<h", data[2:4])[0]/15000
+        z_axis = struct.unpack("<h", data[4:])[0]/15000
+
+        self.data[0] = x_axis
+        self.data[1] = y_axis
+        self.data[2] = z_axis
+        
+        return True
+                
     def getdata_raw(self):
-        self._update_data()
-        return(self.data)
+        flag = self._update_data()
+        
+        return self.data
     
     def compass_2d(self, declination=0):
-        self._update_data()
+        flag = self._update_data()
+        
         # Simple calculation of true heading (if you pass in a declination value) assuming the compass is level
         heading = atan2(self.data[1], -self.data[0])*(180/pi) - declination
         
@@ -91,10 +92,7 @@ class Magnetometer:
         return rx, ry
     
     def compass_3d(self, quat=None, pitch=None, roll=None, declination=0): # quaternion input as list [qw, qx, qy, qz], pitchroll values input in radians - NOT DEGREES
-        self._update_data()
-        
-        if (not quat) and not (pitch or roll): # If you don't pass in quaternion, or pitch AND roll values, then None is returned
-            return None
+        flag = self._update_data() # Updating data
         
         # Checking which format orientation data was passed in - if it's quaternion, then use a quaternion rotation. Otherwise a standard trig rotation is used
         if quat:
@@ -117,9 +115,9 @@ if __name__ == "__main__":
     imu.dmpsetup(2)
     imu.calibrate(10)
     
-    compass = Magnetometer(46, 3, 1)
+    compass = Magnetometer(46, 3)
     
     while True:
         quaternion, orientation, localvel, worldacc = imu.imutrack()
-        print(compass.compass_3d(quat=quaternion, declination=1.43)) # Need to combine with IMU to get orientation data
+        print(compass.compass_3d(quat=quaternion, declination=1.43))
         time.sleep(0.1)
