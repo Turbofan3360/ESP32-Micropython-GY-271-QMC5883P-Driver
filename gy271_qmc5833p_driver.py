050a43395edb29bf8085f020c155bc7e00d8fffc
Samuel Pontefract <zia32kvs@duck.com>
2025-07-26 18:12:53 +0200

    Added calibration functionality
    Sensor can now be calibrated for hard and soft iron effects.

--- gy271_qmc5833p_driver.py
+++ gy271_qmc5833p_driver.py
@@ -21,6 +21,7 @@ class Magnetometer:
             getdata_raw() - returns the raw magnetometer readings in Gauss
             compass_2d(declination=...) - returns a heading rounded to the nearest degree (no pitch/roll compensation). Magnetic declination input optional.
             compass_3d(q, declination=...) - returns a heading rounded to the nearest degree. Fully pitch/roll compensated. Quaternion orientation input required, magnetic declination optional.
+            calibrate(calibrationrotations=...) - enables you to calibrate the sensor. calibrationrotations optional. See the docstring in the calibrate() method for details on the calibration.
         """
         self.qmc5883p = SoftI2C(scl=Pin(scl), sda=Pin(sda), freq=400000)
         self.qmc5883p_address = 0x2C
@@ -39,16 +40,23 @@ class Magnetometer:
                     }
         
         self.data = [0, 0, 0]
+        self.softcal = [1, 1, 1]
+        self.hardcal = [0, 0, 0]
         
         time.sleep_us(250) # Module needs about 250 microseconds to boot up from power on -> being able to receive I2C comms
         
         self._modulesetup()
+    
+    def _log(self, string):
+        print(string)
         
     def _modulesetup(self):
         # Setting the module to Normal power mode, 200Hz data output rate, x4 sensor reads per data output, down sampling = 0 (output every sample)
         self.qmc5883p.writeto_mem(self.qmc5883p_address, self.registers["control1"], bytes([0x1D]))
         # Setting the module to 2 Gauss range, "Set and Reset On" mode
         self.qmc5883p.writeto_mem(self.qmc5883p_address, self.registers["control2"], bytes([0x0C]))
+        
+        self._log("Module setup complete")
     
     def _update_data(self):
         counter = 0
@@ -68,9 +76,9 @@ class Magnetometer:
         y_axis = struct.unpack("<h", data[2:4])[0]/15000
         z_axis = struct.unpack("<h", data[4:])[0]/15000
 
-        self.data[0] = x_axis
-        self.data[1] = y_axis
-        self.data[2] = z_axis
+        self.data[0] = (x_axis - self.hardcal[0]) * self.softcal[0]
+        self.data[1] = (y_axis - self.hardcal[1]) * self.softcal[1]
+        self.data[2] = (z_axis - self.hardcal[2]) * self.softcal[2]
         
         return True
     
@@ -105,7 +113,117 @@ class Magnetometer:
         v3 /= length
         
         return [v1, v2, v3]
+    
+    def _axes_calibration_rotations(self, fieldstrength):
+        x_values, y_values, z_values = [], [], []
+        xcomplete, ycomplete, zcomplete = False, False, False
+        
+        x_values.append(self.data[0])
+        y_values.append(self.data[1])
+        z_values.append(self.data[2])
+        
+        while not(xcomplete and ycomplete and zcomplete):
+            axes_complete = 0
+            flag = self._update_data()
+            
+            if not xcomplete:
+                x_values.append(self.data[0])
+            if not ycomplete:
+                y_values.append(self.data[1])
+            if not zcomplete:
+                z_values.append(self.data[2])
+            
+            if (max(x_values)-min(x_values)) > 0.9*2*fieldstrength and x_values[-1] > x_values[0]-0.1: # Detecting if that axis has had a full rotation and returned to the starting point
+                xcomplete = True
+            if (max(y_values)-min(y_values)) > 0.9*2*fieldstrength and y_values[-1] > y_values[0]-0.1:
+                ycomplete = True
+            if (max(z_values)-min(z_values)) > 0.9*2*fieldstrength and z_values[-1] > z_values[0]-0.1:
+                zcomplete = True
                 
+            time.sleep_ms(10)
+            
+        return x_values, y_values, z_values
+    
+    def _hard_offsets(self, x_values, y_values, z_values, rotations):
+        x_offset, y_offset, z_offset = 0, 0, 0
+        
+        x_offset = (sum(x_values[-rotations:])/rotations + sum(x_values[:rotations])/rotations)/2
+        y_offset = (sum(y_values[-rotations:])/rotations + sum(y_values[:rotations])/rotations)/2
+        z_offset = (sum(z_values[-rotations:])/rotations + sum(z_values[:rotations])/rotations)/2
+        
+        self.hardcal[0] = x_offset
+        self.hardcal[1] = y_offset
+        self.hardcal[2] = z_offset
+    
+    def _soft_offsets(self, x_values, y_values, z_values, rotations):
+        x_offset, y_offset, z_offset = 0, 0, 0
+        
+        x_offset = (sum(x_values[-rotations:])/rotations - sum(x_values[:rotations])/rotations)/2
+        y_offset = (sum(y_values[-rotations:])/rotations - sum(y_values[:rotations])/rotations)/2
+        z_offset = (sum(z_values[-rotations:])/rotations - sum(z_values[:rotations])/rotations)/2
+        
+        offset_avg = (x_offset + y_offset + z_offset)/3
+        
+        x_scale = offset_avg/x_offset
+        y_scale = offset_avg/y_offset
+        z_scale = offset_avg/z_offset
+        
+        self.softcal[0] = x_scale
+        self.softcal[1] = y_scale
+        self.softcal[2] = z_scale
+    
+    def calibrate(self, calibrationrotations=2):
+        """
+        Calibration method for the magnetometer. Removes both hard- and soft-iron effects from the magnetometer readings
+        
+        To calibrate the sensor: Call the calibrate() method, hold the magnetometer and rotate it 360 degrees around ONE of its axes at a time.
+        You will need to rotate the sensor 360 degrees around TWO DIFFERENT axes in order to complete the calibration rotations (i.e. rotate all 3 axes through 360 degrees)
+        
+        Parameters:
+            calibrationrotations - the number of times it will expect you to complete the series of sensor rotations outlined above. Default is 2.
+        
+        No output - calibration values automatically saved and applied to the magnetometer readings.
+        """
+        self._log("Calibrating...")
+        
+        x_values, y_values, z_values = [], [], []
+        fieldstrength = 0
+        number_readings_fieldstrength = 20
+        
+        # Working out an approximate local magnetic field strength
+        for i in range(number_readings_fieldstrength):
+            flag = self._update_data()
+            
+            vector_length = self.data[0]*self.data[0] + self.data[1]*self.data[1] + self.data[2]*self.data[2]
+            vector_length = vector_length**0.5
+            
+            fieldstrength += vector_length
+            
+            time.sleep_ms(5) # sensor operates at 200Hz - 4 milliseconds between new sensor data
+            
+        fieldstrength /= number_readings
+        
+        self._log("Field strength determined")
+        
+        # Finding the average sensor range per axis
+        for i in range(calibrationrotations):
+            x, y, z = self._axes_calibration_rotations(fieldstrength)
+            self._log("Calibration rotation {} complete".format(i))
+            
+            x_values.extend(x) # Adding data from next rotation to the arrays
+            y_values.extend(y)
+            z_values.extend(z)
+        
+        x_values = sorted(x_values)
+        y_values = sorted(y_values)
+        z_values = sorted(z_values)
+        
+        self._hard_offsets(x_values, y_values, z_values, calibrationrotations)
+        self._log("Hard iron calibration complete")
+        
+        self._soft_offsets(x_values, y_values, z_values, calibrationrotations)
+        self._log("Soft iron calibration complete")
+            
     def getdata_raw(self):
         """
         Returns the raw magnetometer data in Gauss. Takes no parameters.
